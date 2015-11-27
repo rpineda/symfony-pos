6e5edd4803a2112b3d4c5b1a45d5a435f463111d
felipe morales <fpilee@gmail.com>
2015-11-27 13:18:34 -0300

    query for list only non deleted entities

--- src/AppBundle/Form/ProductType.php
+++ src/AppBundle/Form/ProductType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 
 class ProductType extends AbstractType
 {
@@ -18,7 +19,15 @@ class ProductType extends AbstractType
             ->add('name', null, ['label' => 'entity.labels.name'])
             ->add('category', 'entity', array('class' => 'AppBundle:Category',
                 'property' => 'name',
-                'label' => 'category.labels.self'
+                'label' => 'category.labels.self',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('p')
+                        ->select('c')
+                        ->from('AppBundle:Category', 'c')
+                        ->where('c.deleted = false')
+                        ;
+                }
+
             ))
             ->add('price', null, ['label' => 'product.labels.price'])
             ->add('cost', null, ['label' => 'product.labels.cost'])
