9ddd53db44e049c5a353f5e1be5fb53c6458a764
felipe morales <fpilee@gmail.com>
2015-12-04 14:06:24 -0300

    default route

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->redirectToRoute('sale');
+
     }
 }
