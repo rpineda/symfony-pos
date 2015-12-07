fa04c9b1ab80c3e02122076d1bc0ac8368a2c8c5
felipe morales <fpilee@gmail.com>
2015-12-07 14:56:08 -0300

    added FOSJsRoutingBundle as dep to avoid hardcode url in code

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new AppBundle\AppBundle(),
         );
 
